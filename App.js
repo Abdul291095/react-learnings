3a26f01061561a90d384e066e83bc10aee2665aa
Abdul291095 <abdulrahman.softsuave@gmail.com>
2024-03-30 23:58:58 +0530

    Lesson - 02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm from h1 tag"),
@@ -11,4 +14,3 @@ const parent = React.createElement("div", { id: "parent" }, [
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-
