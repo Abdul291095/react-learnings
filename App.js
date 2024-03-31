855d181a7038856cbe32086716adc3d8f12fb8ee
Abdul291095 <abdulrahman.softsuave@gmail.com>
2024-03-31 17:54:16 +0530

    Lesson-03

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm from h1 tag"),
-    React.createElement("h2", {}, "I'm from h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm from h1 tag"),
-    React.createElement("h2", {}, "I'm from h2 tag"),
-  ]),
-]);
-console.log(parent);
+const Title = () => <h1 id="heading"> Hello from JSX</h1>;
+
+const Component = () => (
+  <div>
+    <Title />
+    <h1 id="head">Hello from Component</h1>
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<Component />);
